90387ce0a77f8d3ff15fa6c6c92cca8d9cac85ab
fede.caceres <fede.caceres@gmail.com>
2009-07-16 15:43:45 +0000

    Ya se puede setear por linea de comando (tanto en solver.py como en resolver.sh) como ordenar las variables y valores, sintaxis como sigue: solver.py puzzle [--silent] [--var-order minimo|maximo|secuencial] [--val-order menos|mas|secuencial]
    solver.sh puzzle_inicial puzzle_final [minimo|maximo|secuencial [menos|mas|secuencial]]
    
    Acudir a solver.py --help para entender de que se trata cada uno.

--- backtracking/resolver.sh
+++ backtracking/resolver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 COUNTER=$1
 while [  $COUNTER -le $2 ]; do
-     python solver.py $COUNTER --silent
+     python solver.py $COUNTER --silent --var-order $3 --val-order $4
      let COUNTER=COUNTER+1 
 done
 
--- backtracking/solver.py
+++ backtracking/solver.py
@@ -27,6 +27,8 @@ PUZZLES_FILE = "puzzles.txt" # archivo con los puzzless
 HARD_PUZZLES_FILE = "puzzles-hard.txt" # archivo con los puzzless
 MODO_SILENCIOSO = False
 
+VAR_ORDER = VAR_ORDER_SECUENTIAL
+VAL_ORDER = VAL_ORDER_SECUENTIAL
 
 def load_puzzle(n):
     '''Lee el puzzle n del archivo de puzzles'''
@@ -68,8 +70,8 @@ def solve(n):
     puzzle = load_puzzle(n)
     sudo = Sudoku(puzzle)
     sudo.silencioso = MODO_SILENCIOSO
-    sudo.var_order = VAR_ORDER_SECUENTIAL
-    sudo.val_order = VAL_ORDER_SECUENTIAL
+    sudo.var_order = VAR_ORDER
+    sudo.val_order = VAL_ORDER
     if not MODO_SILENCIOSO:
         sudo.printb()
     inicio = datetime.datetime.now()
@@ -102,7 +104,7 @@ if __name__ == '__main__':
         if args[1] == '--help':
             print 'Modo de ejecucion:'
             print '\t' + a + ' --help:  Esta ayuda'
-            print '\t' + a + ' [puzzle]: Resolver un puzzle'
+            print '\t' + a + ' puzzle [opciones]: Resolver un puzzle'
             print
             print '  puzzle: un valor del 1 al 100 donde las dificultades son las siguientes'
             print '    1 al 20   muy facil'
@@ -110,16 +112,59 @@ if __name__ == '__main__':
             print '    41 al 60  medio'
             print '    61 al 80  dificil'
             print '    80 al 100  muy dificil'
-            print '    101 al 195  muy muy muy dificil\n'
+            print '    101 al 195  muy muy muy dificil'
+            print 
+            print '  opciones:'
+            print '    --silent:    Corre en modo silencioso, solo imprime un resumen del trabajo realizado,'
+            print '                 si no se pasa este argumento se imprime el tablero antes y despues de la'
+            print '                 ejecucion.'
+            print '    --var-order criterio: Determina el orden por el cual se escogeran las variables (casillas).'
+            print '                 Puede ser una de las siguientes:'
+            print '                   secuencial: no ordena las casillas'
+            print '                   minimo: elige primero las casillas con menor cantidad de valores restantes'
+            print '                   maximo: elige primero las casilals con mayor cantidad de valores restantes'
+            print '    --var-order criterio: Determina el orden por el cual se escogeran los valores.'
+            print '                 Puede ser una de las siguientes:'
+            print '                   secuencial: no ordena los valores'
+            print '                   menos: elige primero los valores menos conflictivos'
+            print '                   mas: elige primero los valores mas conflictivos'
+                
         else:
             try:
                 n = int(args[1])
             except:
                 exit("Especifico un numero de puzzle incorrecto, no se pudo interpretar como numero")
             else:
+                # argumentos adicionales
+                if len(args) > 2:
+                    next_is_varo = False
+                    next_is_valo = False
+                    for a in args[2:]:
+                        if next_is_varo:
+                            if a == 'secuencial':
+                                VAR_ORDER = VAR_ORDER_SECUENTIAL
+                            elif a == 'minimo':
+                                VAR_ORDER = VAR_ORDER_MINIMUN_REMAINING_VALUES
+                            elif a == 'maximo':
+                                VAR_ORDER = VAR_ORDER_MAXIMUM_REMAINING_VALUES 
+                            next_is_varo = False
+                        elif next_is_valo:
+                            if a == 'secuencial':
+                                VAL_ORDER = VAL_ORDER_SECUENTIAL
+                            elif a == 'menos':
+                                VAL_ORDER = VAL_ORDER_LEAST_CONFLICTS
+                            elif a == 'mas':
+                                VAL_ORDER = VAL_ORDER_MOST_CONFLICTS
+                            next_is_valo = False
+                        elif a == '--silent':
+                            MODO_SILENCIOSO = True
+                        elif a == '--var-order' or a == '-varo':
+                            next_is_varo = True
+                        elif a == '--val-order' or a == '-valo':
+                            next_is_valo = True
+                        
+                # ejecutar
                 if 1 <= n <= 195:
-                    if len(args) == 3 and args[2] == '--silent':
-                        MODO_SILENCIOSO = True
                     solve(n)
                 else:
                     exit("El numero esta fuera de rango, elija uno entre 1 y 100")
