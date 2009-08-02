470f4873ee707f15b2fccde415d211109e3a7b26
sergiogonzalezveron <sergiogonzalezveron@ec9be4fa-5a1c-11de-8888-c79ed7c8b0c3>
2009-08-02 16:37:37 +0000

    Esta mal parece la carga de lista de 0s. Voy a almorzar un rato

--- prolog/pruebas/grassmann.pl
+++ prolog/pruebas/grassmann.pl
@@ -3,6 +3,11 @@
 :- consult('pistas.pl').
 
 
+
+% Devuelve el indice de la lista.
+indexsergio(Fila, Columna, Indice) :- Indice is 9*(Fila-1) + Columna.
+
+
 % pagina 204, predicados recursivos que contienen listas
 
 % procedimiento "miembro"
@@ -25,6 +30,37 @@ anhadir([A | ColaA], B, [A | ColaC]) :- anhadir(ColaA, B, ColaC).
 
 
 % by Sergio
-cargartablerosudoku
+
+% cargar tablero vacio, es decir, llenos de 0s.
+cargartablerovacio(Tablero, 0) :- append(Tablero, [0]).
+cargartablerovacio(Tablero, Cantidadelementos) :-
+    Cantidadelementosmenosuno is Cantidadelementos - 1,
+    cargartablerovacio(Tablero,Cantidadelementosmenosuno)
+
+% recibe el Tablero, que es una lista de 81 elementos
+% Devuelve el Tablero con las pistas cargadas
+cargartablerosudoku([])
 cargartablerosudoku([_|ColaTablero]) :- anhadir(_, pista(X, Y, Z), ColaTablero).
+
+
+
+nth1
+fila, columna, valor
+X, Y, Z
+
+
+
+f(9, 9) = 9*(9-1) + 9
+        = 72 + 9
+        = 81
+
+[1, 2, 3, 4, ..., 9
+ 10, 11, ..., 18,
+ 81]
+
+
+
+KISS
+Keep It Simple, Stupid.
+Dejalo simple, estupido.
 
